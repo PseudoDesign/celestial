842f383a610997c45ca3926432e63f6df001c590
Adam <PseudoDesign@users.noreply.github.com>
2020-03-03 00:30:02 -0700

    changed bash script to python function

--- celestial_tools/client/__init__.py
+++ celestial_tools/client/__init__.py
@@ -1,1 +1,12 @@
+import argparse
+
 from . import rootfs
+
+
+def dual_rootfs_update_cmdline():
+    """
+    Runs a commandline program for updating a dual rootfs
+    :return:
+    """
+    parser = argparse.ArgumentParser(description="Install/verify a dual-rootfs update")
+    parser.add_argument()
