a8dd89593e5f2adc78eb89ae407bf97870dbccc2
Adam <PseudoDesign@users.noreply.github.com>
2020-03-06 21:34:09 -0700

    tweaked sphinx config

--- celestial_tools/client/__init__.py
+++ celestial_tools/client/__init__.py
@@ -6,7 +6,9 @@ from . import rootfs
 def dual_rootfs_update(args: [str]) -> int:
     """
     Runs a commandline program for updating a dual rootfs
+
     :type args: an array of strings dictating the commandline parameters
+
     :return: 0 on success, else a linux error code
     """
     parser = argparse.ArgumentParser(description="Install/verify a dual-rootfs update")
--- celestial_tools/client/dual_rootfs_update_cmdline.py
+++ celestial_tools/client/dual_rootfs_update_cmdline.py
@@ -5,8 +5,10 @@ from celestial_tools.client import dual_rootfs_update
 
 def dual_rootfs_update_cmdline():
     """
-    Runs dual_rootfs_update from the cmdline, parsing the provided sys.argv
-    :return:
+    Runs dual_rootfs_update from the cmdline, parsing the provided sys.argv.
+    setup.py maps this function to `celestial_dual_rootfs_update`.
+
+    One can also execute this file from the commandline to run the commandline script.
     """
     dual_rootfs_update(sys.argv[1:])
 
