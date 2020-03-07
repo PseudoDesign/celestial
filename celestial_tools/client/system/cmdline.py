5aa48249f1ca17a8574849885c033c3e82946ecd
Adam <PseudoDesign@users.noreply.github.com>
2020-03-07 01:17:58 -0700

    updated docstrings

--- celestial_tools/client/system/cmdline.py
+++ celestial_tools/client/system/cmdline.py
@@ -3,11 +3,12 @@ Functions related to the cmdline file
 """
 
 
-def load(cmdline_file):
+def load(cmdline_file) -> str:
     """
-    Load the given file as a string
-    :param cmdline_file:
-    :return:
+    Load the given cmdline file as a string
+
+    :param cmdline_file: The cmdline file to load
+    :return: A string containing the content of the cmdline file
     """
     contents = ""
     with open(cmdline_file, 'r') as fpt:
@@ -16,13 +17,13 @@ def load(cmdline_file):
     return contents
 
 
-def write(parameters, cmdline_file):
+def write(parameters: dict, cmdline_file: str):
     """
     Create a cmdline file with the provided parameters
     Overwrite it if it exists
-    :param parameters:
-    :param cmdline_file:
-    :return:
+
+    :param parameters: A dictionary of key/value pairs
+    :param cmdline_file: The **boot partition's** cmdline file
     """
     data = ""
     for key in parameters:
@@ -33,10 +34,11 @@ def write(parameters, cmdline_file):
         fpt.write(data)
 
 
-def get_parameters(cmdline_file):
+def get_parameters(cmdline_file: str) -> dict:
     """
     Retrieve all of the parameters of the cmdline file
-    :param cmdline_file:
+
+    :param cmdline_file: The cmdline file to load
     :return: a list of {key: , value: }
     """
     contents = load(cmdline_file)
@@ -49,12 +51,12 @@ def get_parameters(cmdline_file):
     return parameters
 
 
-def get_parameter(parameter_name, cmdline_file):
+def get_parameter(parameter_name: str, cmdline_file: str):
     """
     Retrieve a parameter from the provided cmdline file
-    :param parameter_name:
-    :param cmdline_file:
-    :return:
+
+    :param parameter_name: The "key" to get from the cmdline file
+    :param cmdline_file: The **kernel's** cmdline file
     """
     parameters = get_parameters(cmdline_file)
     if "root" in parameters:
@@ -66,10 +68,10 @@ def set_parameter(parameter_name, parameter_value, cmdline_file):
     """
     Set a parameter in the provided cmdline file.  Create it if it doesn't exist,
     overwrite it if it does exist
-    :param parameter_name:
-    :param parameter_value:
-    :param cmdline_file:
-    :return:
+
+    :param parameter_name: The "key" in the key value pair
+    :param parameter_value: The value to set the key  to
+    :param cmdline_file: location of the **boot partition's** cmdline file
     """
     parameters = get_parameters(cmdline_file)
     parameters[parameter_name] = parameter_value
